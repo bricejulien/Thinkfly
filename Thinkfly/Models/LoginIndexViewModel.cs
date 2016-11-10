b47be723178f3594bb75f33d7ad580a94b97b522
Brice Julien <bricejulien27@gmail.com>
2016-11-10 17:51:25 +0100

    First commit

---
+++ Thinkfly/Models/LoginIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Thinkfly.Models
+{
+    public class LoginIndexViewModel
+    {
+        public LoginViewModel LoginModel { get; set; }
+        public IndexViewModel IndexModel { get; set; }
+    }
+}
